dad973df05c7ff3277a35cfc3e782dddf8bbc286
luying <1798028945@qq.com>
2023-03-09 20:36:52 +0800

    v1

---
+++ mysite/background-system/src/urlConfig.js
@@ -0,0 +1,4 @@
+// export const server_URL = 'http://localhost:7001';
+export const server_URL = 'http://47.108.144.102:7001';
+// export const server_URL = 'http://47.108.144.102:27017';
+export const frontEnd_URL = 'http://localhost:8080';
